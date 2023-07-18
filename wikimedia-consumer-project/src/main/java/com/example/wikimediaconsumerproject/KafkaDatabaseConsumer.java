b0d6d484cd3d8af237b14101c7fcbade2ad261fc
Harika Sahini <71969256+harikasahini@users.noreply.github.com>
2023-07-17 23:49:45 -0500

    consumer module completed

---
+++ wikimedia-consumer-project/src/main/java/com/example/wikimediaconsumerproject/KafkaDatabaseConsumer.java
@@ -0,0 +1,19 @@
+package com.example.wikimediaconsumerproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    @KafkaListener(topics = "wikimedia-topic",
+                    groupId = "myGroup")
+    public void consume(String eventMessage){
+        LOGGER.info(String.format("Message Received %s",eventMessage));
+    }
+
+}
