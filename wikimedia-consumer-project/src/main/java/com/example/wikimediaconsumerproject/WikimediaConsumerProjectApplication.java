7e3fe2a47be659e3c75dbf3cb2d98d5518c33fce
Harika Sahini <71969256+harikasahini@users.noreply.github.com>
2023-07-17 23:41:41 -0500

    consumer created

---
+++ wikimedia-consumer-project/src/main/java/com/example/wikimediaconsumerproject/WikimediaConsumerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikimediaconsumerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaConsumerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaConsumerProjectApplication.class, args);
+	}
+
+}
---
+++ wikimedia-consumer-project/src/test/java/com/example/wikimediaconsumerproject/WikimediaConsumerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikimediaconsumerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaConsumerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
